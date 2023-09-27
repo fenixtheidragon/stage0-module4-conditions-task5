5e013a25092832997766cc4705c3891821809419
fenix <fenixtheidragon@gmail.com>
2023-09-27 15:19:39 +0300

    0.4.5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,32 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        String result ="";
+        int remainderFromDivisionBy4 = year % 4;
+        int remainderFromDivisionBy100 = year % 100;
+        int remainderFromDivisionBy400 = year % 400;
+        switch (remainderFromDivisionBy4) {
+            default:
+                result = "not leap";
+                break;
+            case 0:
+                switch (remainderFromDivisionBy100) {
+                    default:
+                        result = "leap";
+                        break;
+                    case 0:
+                        switch (remainderFromDivisionBy400) {
+                            default:
+                                result = "not leap";
+                                break;
+                            case 0:
+                                result = "leap";
+                                break;
+                        }
+                        break;
+                }
+                break;
+        }
+        System.out.println(result);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task5;
 
 public class RangeNumbersPrinter {
     public void printRange(int number) {
+        String result = "";
+        switch(number) {
+            default:
+                result = "out of range";
+                break;
+            case 1,2,3,4,5:
+                result = "number is between 1 and 5";
+                break;
+            case 6,7,8,9,10:
+                result = "number is between 6 and 10";
+                break;
+        }
+        System.out.println(result);
     }
 }
